cf0555f8f75882d52dec214245ba316507490f68
Joshua Kelly <joshuakellyeng@gmail.com>
2023-08-01 21:04:18 -0400

    updated nav and header links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,18 @@ const Footer = () => {
 	return (
 		<div className="footer text-font bg-black flex justify-between p-5 ">
 			<p className="tracking-widest">
-				COPYRIGHT © 2021 LEFT BANK BURGER - ALL RIGHTS RESERVED.
+				COPYRIGHT © 2023 LEFT BANK BURGER - ALL RIGHTS RESERVED.
 			</p>
-			<div className='mod'>
-				<Link className='mx-2' to="/apply">APPLY</Link>
-				<Link className="mx-2" to="/edit-menu">
+			<div className="mod">
+				<Link className="mx-2" to="/apply">
+					Job Application Form
+				</Link>
+				{/* <Link className="mx-2" to="/edit-menu">
 					<i className="fa-solid fa-pen-to-square"></i>
 				</Link>
 				<Link className="mx-2" to="/archive">
 					<i className="fa-solid fa-user"></i>
-				</Link>
+				</Link> */}
 			</div>
 		</div>
 	);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,69 +1,74 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
 	const [navbarOpen, setNavbarOpen] = useState(false);
 	return (
 		<nav className="flex flex-wrap items-center justify-between px-2 py-3 bg-blueGray-500  title-font bg-black text-slate-50 sticky top-0 text-2xl z-50">
-		<div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-			<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-				<Link className="" to="/">
-					<div className="flex lg:text-4xl">
-						<h1 className="">LEFT BANK</h1>
-						<h1 className="brush-font shadow-font px-2 second-font">
-							Burger Bar
-						</h1>
-					</div>
-				</Link>
-				<button
-					className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-					type="button"
-					onClick={() => setNavbarOpen(!navbarOpen)}
+			<div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+				<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+					<Link className="" to="/">
+						<div className="flex lg:text-4xl">
+							<h1 className="">LEFT BANK</h1>
+							<h1 className="brush-font shadow-font px-2 second-font">
+								Burger Bar
+							</h1>
+						</div>
+					</Link>
+					<button
+						className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+						type="button"
+						onClick={() => setNavbarOpen(!navbarOpen)}
+					>
+						<i className="fas fa-bars"></i>
+					</button>
+				</div>
+				<div
+					className={
+						'lg:flex flex-grow items-center' +
+						(navbarOpen ? ' flex' : ' hidden')
+					}
+					id="example-navbar-danger"
 				>
-					<i className="fas fa-bars"></i>
-				</button>
+					<ul className="flex flex-col lg:flex-row list-none lg:ml-auto text-xl">
+						<li className="nav-item">
+							<Link
+								className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
+								to="/menu"
+								onClick={() => setNavbarOpen(!navbarOpen)}
+							>
+								MENU
+							</Link>
+						</li>
+						<li className="nav-item">
+							<a
+								className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
+								href="https://www.grubhub.com/restaurant/left-bank-burger-bar-194-newark-ave-jersey-city/293389"
+							>
+								ORDER
+							</a>
+						</li>
+						<li className="nav-item">
+							<Link
+								className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
+								to="/contact"
+								onClick={() => setNavbarOpen(!navbarOpen)}
+							>
+								CONTACT US
+							</Link>
+						</li>
+						<li className="nav-item">
+							<a
+								className="px-3 pt-3 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
+								href="https://www.instagram.com/lb.downtown/"
+							>
+								<i className="mx-2 fab fa-instagram text-lg leading-lg text-white opacity-75"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div
-				className={
-					'lg:flex flex-grow items-center' +
-					(navbarOpen ? ' flex' : ' hidden')
-				}
-				id="example-navbar-danger"
-			>
-				<ul className="flex flex-col lg:flex-row list-none lg:ml-auto text-xl">
-					<li className="nav-item">
-						<Link
-							className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
-							to="/menu"
-						onClick={()=>setNavbarOpen(!navbarOpen)}>MENU</Link>
-					</li>
-					<li className="nav-item">
-						<a
-							className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
-							href="https://www.grubhub.com/restaurant/left-bank-burger-bar-194-newark-ave-jersey-city/293389"
-						>
-							ORDER
-						</a>
-					</li>
-					<li className="nav-item">
-						<Link
-							className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
-							to="/contact"
-							onClick={()=>setNavbarOpen(!navbarOpen)}
-						>CONTACT US</Link>
-					</li>
-					<li className="nav-item">
-						<a
-							className="px-3 pt-3 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
-							href="https://www.instagram.com/lb_burgerbar/"
-						>
-							<i className="mx-2 fab fa-instagram text-lg leading-lg text-white opacity-75"></i>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+		</nav>
 	);
 };
 
